dbfd07df7957e2e36d0fbb717d67b4c2af31c005
othiel <othiel.leith@gmail.com>
2023-09-14 17:28:04 +0200

    homepage done

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,26 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable react/prop-types */
+import React from "react";
+import noimage from "../assets/images/no-image.jpg";
+
+const getImage = (path) => `https://image.tmdb.org/t/p/w300/${path}`;
+export default function Home({ movie }) {
+  if (!movie) {
+    return <h1>appuyer bouton</h1>;
+  }
+  return (
+    <div className="card_movie">
+      {/* <img src={getImage(movie.poster_path)} alt={movie.original_title} /> */}
+      {movie.poster_path === null ? (
+        <img src={noimage} />
+      ) : (
+        <img src={getImage(movie.poster_path)} />
+      )}
+
+      <h4>{movie.original_title}</h4>
+      <p>
+        {movie.vote_average} <span>&#9733;</span>
+      </p>
+    </div>
+  );
+}
---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,128 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+// import { Link } from "react-router-dom";
+import Home from "../components/Home";
+import "../styles/Homepage.css";
+
+const apiKey = import.meta.env.VITE_API_KEY;
+export default function HomePage1() {
+  const settings = {
+    infinite: false,
+    speed: 500,
+    slidesToShow: 8,
+    slidesToScroll: 4,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 6,
+          slidesToScroll: 3,
+          infinite: true,
+          // dots: true,
+        },
+      },
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 3,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 750,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 3,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 464,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+        },
+      },
+    ],
+  };
+
+  const [newMovie, setNewMovie] = useState([]);
+  const [fantasyMovie, setFantasyMovie] = useState([]);
+  const [adventureMovie, setAdventureMovie] = useState([]);
+
+  const getNewMovie = () => {
+    axios
+      .get("https://api.themoviedb.org/3/movie/upcoming", {
+        params: { apiKey },
+      })
+      .then((response) => {
+        setNewMovie(response.data.results);
+      });
+  };
+  const getFantasyMovie = () => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=comedy&include_adult=false&language=fr-FR&page=1`
+      )
+      .then((response) => {
+        setFantasyMovie(response.data.results);
+      });
+  };
+  const getAdventureMovie = () => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=action&include_adult=false&language=fr-FR&page=1`
+      )
+      .then((response) => {
+        setAdventureMovie(response.data.results);
+      });
+  };
+  useEffect(() => {
+    getNewMovie();
+    getFantasyMovie();
+    getAdventureMovie();
+  }, []);
+  return (
+    <>
+      <div className="header_nouveautés">
+        <h3>Nouveautés</h3>
+        <div className="cards">
+          <Slider {...settings}>
+            {newMovie.map((movie) => (
+              <Home key={movie.id} movie={movie} />
+            ))}
+          </Slider>
+        </div>
+      </div>
+
+      <div className="card_comédie">
+        <h3>Comédie</h3>
+        <div className="cards">
+          <Slider {...settings}>
+            {fantasyMovie.map((movie) => (
+              <Home key={movie.id} movie={movie} />
+            ))}
+          </Slider>
+        </div>
+      </div>
+
+      <div className="card_action">
+        <h3>Action</h3>
+        <div className="cards">
+          <Slider {...settings}>
+            {adventureMovie.map((movie) => (
+              <Home key={movie.id} movie={movie} />
+            ))}
+          </Slider>
+        </div>
+      </div>
+    </>
+  );
+}
