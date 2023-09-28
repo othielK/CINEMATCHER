8aff41f0629187c413f587a6a9cc61449450486b
othiel <othiel.leith@gmail.com>
2023-09-28 15:22:31 +0200

    fixed genre issues and pagination

--- frontend/src/components/Content.jsx
+++ frontend/src/components/Content.jsx
@@ -4,7 +4,8 @@ import Favoris from "../pages/Favoris";
 import NouveautesEtPopulaires from "../pages/NouveautesEtPopulaires";
 import Humeurs from "../pages/Humeurs";
 import MovieDetails from "../pages/MovieDetails";
-import Genres from "../pages/Genres";
+// import Genres from "./Genres";
+import GenreMovies from "../pages/GenreMovies";
 
 export default function Content() {
   return (
@@ -14,7 +15,7 @@ export default function Content() {
       <Route path="/favoris" element={<Favoris />} />
       <Route path="/newandpopular" element={<NouveautesEtPopulaires />} />
       <Route path="/humeurs" element={<Humeurs />} />
-      <Route path="/genres" element={<Genres />} />
+      <Route path="/genres" element={<GenreMovies />} />
     </Routes>
   );
 }
