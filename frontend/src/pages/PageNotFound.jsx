70e8e1f029a756f83088bcb6dc81af20797beb00
Malika Bounafaa-Kerbourc'h <mbounafaa.mb@gmail.com>
2023-10-05 11:01:52 +0100

    add GIF page not found

--- frontend/src/pages/PageNotFound.jsx
+++ frontend/src/pages/PageNotFound.jsx
@@ -4,6 +4,7 @@ import "../styles/Pagenotfound.css";
 export default function NotFound() {
   return (
     <div className="Oops">
+      <iframe title="404 image" src="https://giphy.com/embed/rs1er4JfkiRtm" />
       <h1>Oops ! Tu sembles perdu.e</h1>
       <Link className="cta-homepage" to="/">
         Accueil
